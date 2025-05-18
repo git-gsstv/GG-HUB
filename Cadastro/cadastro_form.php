de98c8e96c8739aadde53ffa6e23c22f4b137556
git-gsstv <gcoutinho26oliveira@gmail.com>
2025-05-18 15:59:35 -0300

    Formulário atualizado com estilização

--- Cadastro/cadastro_form.php
+++ Cadastro/cadastro_form.php
@@ -4,15 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GG HUB</title>
+    <link rel="stylesheet" href="../Estilização/styleGGHUB.css">
 </head>
 <body>
-    <form action="cadastro.php" method="post">
-        <input type="hidden" name="act" value="save">
-        <input type="hidden" name="id_adm" id="id_adm">
-        <input type="text" name="usuario" id="usuario" placeholder="nome de usuário">
-        <input type="email" name="email" id="email" placeholder="email">
-        <input type="password" name="senha" id="senha" placeholder="senha">
-        <input type="submit" value="Cadastrar">
-    </form>
+    <div class="container">
+        <div class="painelEsquerdo">
+            <img src="../Estilização/Assets/GGHUBlogo.png" alt="GG HUB Logo" class="logo">
+            <h2>CRIE SUA<br>CONTA</h2>
+        </div>
+        <div class="painelDireito">
+            <form action="cadastro.php" method="post">
+
+                <input type="hidden" name="act" value="save">
+                <input type="hidden" name="id_adm" id="id_adm">
+                
+                <div class="inputFormulario">
+                    <img src="../Estilização/Assets/userIcon.png" alt="Icon do Usuário" class="inputIcon">
+                    <input type="text" name="usuario" placeholder="nome de usuário" class="inputArea" required>
+                </div>
+                <div class="inputFormulario">
+                    <img src="../Estilização/Assets/emailIcon.png" alt="Icon do Email" class="inputIcon">
+                    <input type="email" name="email" placeholder="email" class="inputArea" required>
+                </div>
+                <div class="inputFormulario">
+                    <img src="../Estilização/Assets/senhaIcon.png" alt="Icon da Senha" class="inputIcon">
+                    <input type="password" name="senha" placeholder="senha" class="inputArea" required>
+                </div>
+                <input type="submit" value="CADASTRAR" id="cadBotao">
+                <p id="loginBotao">fazer login</p>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
